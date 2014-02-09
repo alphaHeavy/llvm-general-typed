eb956884f9a9ccfa81b7ca0b627493652602e1fe
Nathan Howell <nhowell@alphaheavy.com>
2014-02-08 16:33:21 -0800

    AST.CC and Typed.CC are isomorphic

--- CallingConv.hs
+++ CallingConv.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module CallingConv where
 
+import Data.Proxy
 import GHC.TypeLits
+import qualified LLVM.General.AST.CallingConvention as CC
 
 data CallingConv where
   CallingConv :: Nat -> CallingConv
@@ -17,3 +21,34 @@ type X86_StdCall  = 'CallingConv 64
 type X86_FastCall = 'CallingConv 65
 type X86_64_Win64 = 'CallingConv 79
 
+reifyCallingConv
+  :: forall proxy nat . KnownNat nat
+  => proxy ('CallingConv nat)
+  -> CC.CallingConvention
+reifyCallingConv _ = case natVal (Proxy :: Proxy nat) of
+  0  -> CC.C
+  8  -> CC.Fast
+  9  -> CC.Cold
+  10 -> CC.GHC
+  x  -> CC.Numbered (fromIntegral x)
+
+promoteCallingConv
+  :: forall r
+   . CC.CallingConvention
+  -> (forall proxy nat . KnownNat nat => proxy ('CallingConv nat) -> r)
+  -> Maybe r
+promoteCallingConv cc f = case cc of
+  CC.C    -> Just (f (Proxy :: Proxy C))
+  CC.Fast -> Just (f (Proxy :: Proxy Fast))
+  CC.Cold -> Just (f (Proxy :: Proxy Cold))
+  CC.GHC  -> Just (f (Proxy :: Proxy GHC))
+  CC.Numbered n -> do
+    SomeNat witness <- someNatVal (fromIntegral n)
+    return $ promoteNumbered witness f
+
+promoteNumbered
+  :: forall proxy nat r
+   . proxy nat
+  -> (Proxy ('CallingConv nat) -> r)
+  -> r
+promoteNumbered _ f = f Proxy
