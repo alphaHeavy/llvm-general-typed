05421da081455f5c4644e1636ba969720896e264
Nathan Howell <nhowell@alphaheavy.com>
2014-05-17 13:40:07 -0700

    Move everything under a new subdir

--- llvm-general-typed-pure/x.hs
+++ llvm-general-typed-pure/x.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Applicative
 import Control.Monad
 import Data.Int
+import Data.Proxy
 import qualified LLVM.General.AST as AST
 import LLVM.General.PrettyPrint (showPretty)
 
+import LLVM.General.Typed
+import LLVM.General.Typed.CallingConv
 import LLVM.General.Typed.DefineBasicBlock
 import LLVM.General.Typed.Module
 import LLVM.General.Typed.Instructions
@@ -22,14 +26,14 @@ foo = do
       val = 42 + 9
 
   namedModule "foo" $ do
-    void . namedFunction "bar" $ mdo
+    _x :: Function C (Proxy [Int, Int]) <- namedFunction_ "bar" $ mdo
       entryBlock <- basicBlock_ $ do
         br secondBlock
 
       secondBlock <- namedBasicBlock_ (AST.Name "second") $ do
         someLocalPtr <- alloca
-        store someLocalPtr (99 :: Value 'Constant Int8)
-        someLocal <- load someLocalPtr
+        store True someLocalPtr (99 :: Value 'Constant Int8)
+        someLocal <- load True someLocalPtr
         x <- val `add` someLocal
         join $ condBr
           <$> cmp someLocal (mutable 99)
@@ -37,6 +41,8 @@ foo = do
           <*> basicBlock_ (br entryBlock)
 
       return ()
+
+    return ()
 
 main :: IO ()
 main = do
